6b7629e054bd15ba1cdaea311ed2730625f463aa
Björn Fällbom <bjorn.fallbom@artvise.se>
2025-06-21 13:56:09 +0200

    email_validator: Use email_validator lib instead of regex for validagin emails in app

--- email-slicer.py
+++ email-slicer.py
@@ -1,27 +1,13 @@
-import re
+from email_validator import validate_email, EmailNotValidError
 
 
-def validate_email(email: str) -> bool:
-    email_regex = re.compile(
-        r"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+"
-        r"(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|"
-        r"((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?"
-        r"(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]"
-        r"|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|"
-        r"(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|"
-        r"[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*"
-        r"(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@"
-        r"((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|"
-        r"(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])"
-        r"([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*"
-        r"([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+"
-        r"(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|"
-        r"(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])"
-        r"([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*"
-        r"([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$",
-        re.IGNORECASE,
-    )
-    return email_regex.match(email) is not None
+def validate_email_internal(email: str) -> bool:
+    try:
+        validate_email(email)
+        return True
+    except EmailNotValidError as e:
+        print(str(e))
+        return False
 
 
 def get_user_email() -> str:
@@ -30,15 +16,13 @@ def get_user_email() -> str:
 
     while not email_valid:
         email = input("Enter your email address: ").strip()
-        email_valid = validate_email(email)
-        if not email_valid:
-            print("Invalid email format. Try again..")
+        email_valid = validate_email_internal(email)
 
     return email
 
 
 def get_split_email(email: str) -> tuple[str, str, str]:
-    if not validate_email(email):
+    if not validate_email_internal(email):
         raise Exception("Invalid e-mail address")
 
     username, domain_ext = email.split("@")
